ebaef6709bdff77d6ad4b890bb656534a36f1c50
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2023-05-24 10:53:40 -0300

    att estudos

---
+++ ex009/ex001.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log (`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 120){
+    console.log (`Voce ultrapassou a velocidade da luz. É O BRIAN?`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ ex009/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Voce e brasileiro')
+} else {
+    console.log('Voce e estrangeiro')
+}
